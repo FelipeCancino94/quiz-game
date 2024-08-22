41c365af4ecab90b3d7f4e0838ea3f7a70ffffc6
Felipe Cancino <jfelipecr94@gmail.com>
2024-08-22 19:35:30 -0400

    change app next to react create app and add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import FormQuiz from './FormQuiz/FormQuiz';
+
+function App() {
+  return (
+    <div className="App">
+      <FormQuiz />
+    </div>
+  );
+}
+
+export default App;
